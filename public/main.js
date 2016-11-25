919637d75e7321676cf2334ca0d41b97fb804d68
sara toftegaard <satoftegaard@gmail.com>
2016-11-25 16:28:45 -0500

    drop falling

--- public/main.js
+++ public/main.js
@@ -1,6 +1,35 @@
-const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+const qs = s => document.querySelector(s)
+
+// const main = () => {
+  // document.getElementById('root').textContent = 'Hello, World!'
+// const main = () => {
+//   const drop = qs('.drop')
+//
+//   drop.style.top = '0px'
+//
+//   setInterval(() => {
+//     drop.style.top = '0px'
+//   }, 6000)
+//
+//   const animate = () => {
+//     drop.style.top = `${parseInt(drop.style.top) + 1}px`
+//   }
+//
+//   setInterval(animate, 14)
+// }
+
+const drop = document.querySelector('.drop')
+
+setInterval(() => {
+  drop.style.top = '0px'
+}, 1000)
+
+const animate = () => {
+  drop.style.top = `${parseInt(drop.style.top) + 3}px`
 }
 
-document.addEventListener('DOMContentLoaded', main)
+setInterval(animate, 3)
 
+object.addEventListener('click', drop)
+
+document.addEventListener('DOMContentLoaded', drop)
